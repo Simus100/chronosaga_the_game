25eea51fff87b5d27b62abbb2cfedf3d7d6ceb6e
Simone Macelloni <admin@universalis.it>
2026-08-20 18:55:55 +0200

    feat: export systemic world foundation APIs

--- packages/game-core/src/index.ts
+++ packages/game-core/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./state/create-campaign";
+export * from "./state/create-systemic-scenario";
+export * from "./state/validate-systemic-state";
+export * from "./state/delayed-consequences";
 export * from "./events/eligibility";
 export * from "./events/select-event";
 export * from "./events/resolve-choice";
